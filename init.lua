8dc3852fc0ef4ce6a761a926d82909619965fb0f
mathias234 <mathiasda98@proton.me>
2024-01-08 23:23:50 +0100

    Update config

--- init.lua
+++ init.lua
@@ -16,19 +16,16 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.relativenumber = true
 vim.opt.number = true
 
-if vim.g.neovide then
-  vim.opt.guifont = "BerkeleyMono Nerd Font:h10"
-  vim.g.neovide_scale_factor = 1.0
-  local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-  end
-  vim.keymap.set("n", "<C-+>", function()
-    change_scale_factor(1.25)
-  end)
-  vim.keymap.set("n", "<C-->", function()
-    change_scale_factor(1/1.25)
-  end)
-
+vim.opt.guifont = "BerkeleyMono Nerd Font:h10"
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
+vim.keymap.set("n", "<C-+>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1/1.25)
+end)
 
 require("lazy").setup("plugins", {})
